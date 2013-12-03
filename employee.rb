d9c0b0d7009656123ef0478f7b2042a72fc404cd
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2013-12-03 16:51:39 -0500

    First commit

---
+++ employee.rb
@@ -0,0 +1,7 @@
+class Employee
+  def self.import_employee_list
+  end
+
+  def self.display_employee_list
+  end
+end
---
+++ sale.rb
@@ -0,0 +1,7 @@
+class Sale
+  def self.import_monthly_sales_list
+  end
+
+  def self.display_monthly_sales_list
+  end
+end
